c1ae4cd31d39cf0437620d868502feaaad1450f7
dylan <dylannelson58@gmail.com>
2021-03-08 20:13:38 -0700

    Add project files.

---
+++ RolloftheDice_ListBox.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RolloftheDice_ListBox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DiceOutcomeTextBox = New System.Windows.Forms.TextBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DiceOutcomeTextBox
+        '
+        Me.DiceOutcomeTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.DiceOutcomeTextBox.Multiline = True
+        Me.DiceOutcomeTextBox.Name = "DiceOutcomeTextBox"
+        Me.DiceOutcomeTextBox.Size = New System.Drawing.Size(429, 354)
+        Me.DiceOutcomeTextBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(12, 372)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(139, 66)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(157, 372)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(139, 66)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(302, 372)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(139, 66)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RolloftheDice_ListBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.DiceOutcomeTextBox)
+        Me.Name = "RolloftheDice_ListBox"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DiceOutcomeTextBox As TextBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RolloftheDice_ListBox.vb
@@ -0,0 +1,69 @@
+﻿Option Strict On
+Option Explicit On
+
+'Dylan Nelson
+'RCET0265
+'Spring 2021
+'Roll of the Dice
+'https://github.com/dylannelson58/RolloftheDice
+
+Public Class RolloftheDice_ListBox
+
+    Dim randomNumbers(10) As Integer
+    Dim header As Integer
+    Dim result As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Formatting()
+
+    End Sub
+
+
+    Private Function GetRandomNumber(maxNumber As Integer) As Integer                               'function to get a random number for 2-12
+        Randomize(DateTime.Now.Millisecond)                                                         'randomizes the number down to the current milisecond
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))                                            'returns a random number equal to or less than the maxNumber + 1
+    End Function                                                                                    'ends function
+
+    Private Function GetSecondRandomNumber(maxNumber As Integer) As Integer                         'function to get a random number for 2-12
+        Randomize(DateTime.Now.Millisecond)                                                         'randomizes the number down to the current milisecond
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))                                            'returns a random number equal to or less than the maxNumber + 1
+    End Function
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+
+        DiceOutcomeTextBox.Text = ""
+
+        For i = 0 To 1000                                                                           'executes 1000 rolls of the two dice
+            result = GetRandomNumber(5) + GetSecondRandomNumber(5)                                  'adds the two functions into a variable that can be called later
+            randomNumbers(result) += 1                                                              'adds 1 to the formatting ensuring that 0-1 is never recorded
+        Next
+
+        Formatting()
+
+    End Sub
+
+
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+        Formatting()
+    End Sub
+
+    Public Function Formatting() As Integer
+        header = CInt((110 - 16) / 2)                                                                '77 because that is the amount of "-"s used 16 is the length of "roll of the dice" phrase /2 to center the header
+        DiceOutcomeTextBox.Text &= (StrDup(header, " ") & "Roll of the Dice" & StrDup(header, " ")) & vbNewLine & _            'Applying the header variable to the "roll of the dice"
+        (StrDup(110, "-")) & vbNewLine                                                                                                       'Adding dashes to format
+
+        For i = 2 To 12                                                                             '2-12 is the combinations of possible dice sum outcomes 
+            DiceOutcomeTextBox.Text &= (Str(i).PadLeft(8) & "|")                                                  'More formatting
+        Next                                                                                        'ends the for loop
+
+        DiceOutcomeTextBox.Text &= vbNewLine & (StrDup(110, "-")) & vbNewLine                                                                                         'ends the line
+
+        For i = 0 To UBound(randomNumbers)
+            DiceOutcomeTextBox.Text &= (CStr(randomNumbers(i)).PadLeft(8) & "|")
+        Next
+    End Function
+
+End Class
